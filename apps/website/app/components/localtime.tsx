f119a6a76f103b6611d3ff73d6a1b013210057e1
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-12-26 12:33:01 +1100

    Update localtime.tsx

--- apps/website/app/components/localtime.tsx
+++ apps/website/app/components/localtime.tsx
@@ -1,9 +1,14 @@
 import TooltipText from "./tooltip-text";
+import { headers } from 'next/headers'
 
 export default function LocalTime({ time, className, type }: { time: Date, className?: string, type: "normal" | "hour-min" }) {
+    const tz = headers().get("x-vercel-ip-timezone")
+    if (tz) {
+        time = new Date(time.toLocaleString("en-US", { timeZone: tz }))
+    }
 
     return (
-        <TooltipText text={time.toLocaleString()}>
+        <TooltipText text={`${time.toLocaleString()} (${tz})`}>
             <time dateTime={time.toISOString()} className={className}>
                 {type === "normal" ? time.toLocaleTimeString()
                     : type === "hour-min" ? time.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' })
