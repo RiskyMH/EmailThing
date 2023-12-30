a827a4e1c15f4d4ecd9fac035e1cf95ba7c70c1b
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-12-30 20:51:06 +1100

    update

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -4,13 +4,14 @@ const nextConfig = {
         // ppr: true,
     },
     async headers() {
+        const domains = `https://riskymh.dev https://emailthing.xyz/ ${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'} ${process.env.VERCEL_URL || ''} ${process.env.VERCEL_BRANCH_URL || ''}`
         return [
             {
                 source: '/mail/:path*',
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: `default-src https://riskymh.dev https://emailthing.xyz/ ${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'} ${process.env.VERCEL_URL || ''} ${process.env.VERCEL_BRANCH_URL || ''}`,
+                        value: `img-src ${domains}; font-src ${domains}; style-src ${domains};`,
                     }
 
                 ],
