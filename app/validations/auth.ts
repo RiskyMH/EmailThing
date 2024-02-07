d81732a0b48955deed838e4ed8ea60b0f1b967c1
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-02-07 16:53:52 +1100

    20 char username

--- app/validations/auth.ts
+++ app/validations/auth.ts
@@ -3,7 +3,7 @@ import * as z from "zod"
 export const userAuthSchema = z.object({
     username: z.string()
         .min(4, "Username needs to be at least 4 characters")
-        .max(15, "Username can't be longer than 15 characters")
+        .max(20, "Username can't be longer than 20 characters")
         .regex(/^[a-zA-Z0-9]+$/, "Username can only contain letters and numbers"),
 
     password: z.string()
