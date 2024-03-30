03e229f393e6ae660f86fe1f1dbdf265ee9d41ba
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-03-30 13:25:38 +1100

    uh

--- app/(auth)/register/action.ts
+++ app/(auth)/register/action.ts
@@ -73,7 +73,7 @@ export default async function signUp(data: FormData): Promise<{ error?: string |
 
         db.insert(Mailbox)
             .values({
-                id: createId(),
+                id: mailboxId,
             }),
 
         db.insert(MailboxForUser)
