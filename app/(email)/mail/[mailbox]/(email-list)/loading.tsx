2efbfbf730ab782d1cdfe81dfb60a27fde2839b4
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-05-09 13:16:34 +1000

    ^

--- app/(email)/mail/[mailbox]/(email-list)/loading.tsx
+++ app/(email)/mail/[mailbox]/(email-list)/loading.tsx
@@ -2,7 +2,7 @@
 export default function Loading() {
     return (
         <div className="flex flex-col gap-2 p-5 w-full min-w-0">
-            <div className="sm:flex flex-row w-full min-w-0 pb-3 border-b-2 -mt-1 gap-6 hidden animate-pulse">
+            <div className="flex flex-row w-full min-w-0 pb-3 border-b-2 -mt-1 gap-6 animate-pulse">
                 <input type="checkbox" disabled id="select" className="mr-2 self-start mt-1 h-4 w-4" />
                 <div className="inline-flex items-center gap-1 px-1 max-w-fit w-auto font-bold border-b-3 border-transparent">
                     <span className="self-center w-36 bg-muted-foreground/25 h-6 rounded" />
