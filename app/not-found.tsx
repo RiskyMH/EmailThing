b51ef80882a0d4ddd8417f9158f94f0fa6127ba1
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-05-14 13:28:53 +1000

    remove the subtext in 404 as its semi solved (with router.refresh)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,6 @@ export default function NotFound() {
         <div className="flex h-screen w-full items-center justify-center flex-col">
             <h1 className="text-4xl font-bold text-foreground">Not found</h1>
             <p className="text-muted-foreground">The page you are looking for does not exist</p>
-            <p className="text-muted-foreground text-sm">If you just signed in, please refresh the page</p>
         </div>
     )
 }
