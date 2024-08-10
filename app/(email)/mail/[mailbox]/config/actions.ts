aac10746e8914c7ddae1e38e32784a07c06f345a
Anay Paraswani <me@anayparaswani.dev>
2024-08-10 17:27:44 +0530

    Even I made a dumb mistake

--- app/(email)/mail/[mailbox]/config/actions.ts
+++ app/(email)/mail/[mailbox]/config/actions.ts
@@ -163,7 +163,7 @@ export async function addAlias(mailboxId: string, alias: string, name: string | 
         if (emailPart.length <= 3) {
             return { error: "Email too short" }
         }
-        else if (impersonatingEmails.toLowerCase().some(v => emailPart.includes(v))) {
+        else if (impersonatingEmails.some(v => emailPart.toLowerCase().includes(v))) {
             return { error: "Invalid alias" }
         }
     }
