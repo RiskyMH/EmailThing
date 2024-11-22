4a28bc5acbfe0f038a6d8442e2d45eafcd2aa669
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-11-22 14:36:11 +1100

    ts

--- app/(email)/mail/[mailbox]/[email]/parse-html.tsx
+++ app/(email)/mail/[mailbox]/[email]/parse-html.tsx
@@ -4,8 +4,12 @@ import { JSDOM } from "jsdom";
 
 const DOMPurify = createDOMPurify((globalThis as any).window || new JSDOM("").window);
 
+function isElement(node: Node): node is Element {
+    return node.nodeType === node.ELEMENT_NODE;
+}
+
 DOMPurify.addHook("afterSanitizeAttributes", (node) => {
-    if (!("setAttribute" in node)) return
+    if (!isElement(node)) return
 
     // set all elements owning target to target=_blank
     if ("target" in node) {
