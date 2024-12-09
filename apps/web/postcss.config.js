e1a048dab19c01473a0e2c06b3e0a22a845ba88b
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-09 19:52:37 +1100

    uhmm

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,14 +1,5 @@
-// const path = require("path")
-// console.log(process.env.npm_config_local_prefix)
-// const hexFixPath = (path.join(process.env.npm_config_local_prefix || __dirname || "./", "./postcss/hex-fix.cjs"));
-
-// const hexFixPath = require.resolve("./postcss/hex-fix.cjs")
-
 module.exports = {
   plugins: {
-    // [hexFixPath]: {},
-    // tailwindcss: {},
-    // autoprefixer: {},
     '@tailwindcss/postcss': {},
   },
 };
