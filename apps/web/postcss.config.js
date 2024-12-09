93333e5a086a6672a0eb514c2583e4b60cd3f2e8
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-09 19:17:58 +1100

    tailwind v4 beta
    what is stability?

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -2,12 +2,13 @@
 // console.log(process.env.npm_config_local_prefix)
 // const hexFixPath = (path.join(process.env.npm_config_local_prefix || __dirname || "./", "./postcss/hex-fix.cjs"));
 
-const hexFixPath = require.resolve("./postcss/hex-fix.cjs")
+// const hexFixPath = require.resolve("./postcss/hex-fix.cjs")
 
 module.exports = {
   plugins: {
-    [hexFixPath]: {},
-    tailwindcss: {},
-    autoprefixer: {},
+    // [hexFixPath]: {},
+    // tailwindcss: {},
+    // autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
--- packages/db/db.ts
+++ packages/db/db.ts
@@ -1,12 +1,11 @@
-import { env } from "@/utils/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
 // create the connection
 const client = createClient({
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_TOKEN,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN!,
 });
 const db = drizzle(client, { schema, logger: false });
 
