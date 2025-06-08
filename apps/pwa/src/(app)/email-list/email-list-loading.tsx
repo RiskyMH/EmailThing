b13ab968250d735c0c1963fd549c5b5bc5fb6329
RiskyMH <git@riskymh.dev>
2025-06-09 00:48:00 +1000

    Update email-list-loading.tsx

--- apps/pwa/src/(app)/email-list/email-list-loading.tsx
+++ apps/pwa/src/(app)/email-list/email-list-loading.tsx
@@ -7,8 +7,10 @@ export default function Loading() {
         <div className="overflow z-10 flex h-10 w-full min-w-0 flex-row items-center justify-center gap-2 //overflow-y-hidden border-b-2 bg-card px-4 sm:[.emailscolumn_&]:rounded-t-lg sm:rounded-tl-lg">
           <EmailListCategoryLoadingSkeleton />
         </div>
-        <div className="flex flex-col h-full overflow-y-auto overflow-x-hidden w-full bg-card pt-2 px-2 min-w-0 gap-2" id="email-list-content">
-          <EmailListLoadingSkeleton />
+        <div className="flex flex-col h-full overflow-y-auto overflow-x-hidden w-full bg-card pt-2 px-2" id="email-list-content">
+          <div className="infinite-scroll-component__outerdiv flex w-full min-w-0 flex-col gap-2">
+            <EmailListLoadingSkeleton />
+          </div>
         </div>
       </div>
       <div className="min-w-0 flex-col h-full //p-3 sm:[.emailscolumn_&]:flex hidden w-1/2 lg:w-3/5 rounded-lg overflow-auto bg-card">
@@ -25,31 +27,33 @@ export default function Loading() {
 
 export function EmailListLoadingSkeleton() {
   return (
-    <div className="infinite-scroll-component__outerdiv">
+    <>
       <div className="flex h-5 px-2 font-medium text-muted-foreground text-xs">
         <p className="self-end">Today</p>
       </div>
 
-      {Array.from({ length: 25 }).map((_, i) => (
-        <div
-          key={i}
-          className="//bg-card flex h-10 w-full animate-pulse gap-3 rounded-md px-2 py-1.5"
-        >
-          <span
-            className="m-2 mx-auto inline-block size-4 shrink-0 self-center rounded-full"
-            style={{ backgroundColor: "grey" }}
-          />
-          <StarIcon className="hidden size-4 shrink-0 self-center text-muted-foreground sm:inline-block" />
-          <div className="h-4 w-1/4 shrink-0 self-center sm:w-32 md:w-56 sm:[.emailscolumn_&]:w-32">
-            <span className="me-auto block h-4 w-full max-w-20 rounded bg-muted-foreground/50" />
+      {
+        Array.from({ length: 25 }).map((_, i) => (
+          <div
+            key={i}
+            className="//bg-card flex h-10 w-full animate-pulse gap-3 rounded-md px-2 py-1.5"
+          >
+            <span
+              className="m-2 mx-auto inline-block size-4 shrink-0 self-center rounded-full"
+              style={{ backgroundColor: "grey" }}
+            />
+            <StarIcon className="hidden size-4 shrink-0 self-center text-muted-foreground sm:inline-block" />
+            <div className="h-4 w-1/4 shrink-0 self-center sm:w-32 md:w-56 sm:[.emailscolumn_&]:w-32">
+              <span className="me-auto block h-4 w-full max-w-20 rounded bg-muted-foreground/50" />
+            </div>
+            <div className="h-4 w-full self-center">
+              <span className="me-auto block h-4 w-full max-w-56 rounded bg-muted-foreground/50 md:max-w-72 lg:max-w-[30rem]" />
+            </div>
+            <span className="float-right h-4 w-10 shrink-0 self-center rounded bg-muted-foreground/25 text-right text-sm sm:w-16" />
           </div>
-          <div className="h-4 w-full self-center">
-            <span className="me-auto block h-4 w-full max-w-56 rounded bg-muted-foreground/50 md:max-w-72 lg:max-w-[30rem]" />
-          </div>
-          <span className="float-right h-4 w-10 shrink-0 self-center rounded bg-muted-foreground/25 text-right text-sm sm:w-16" />
-        </div>
-      ))}
-    </div>
+        ))
+      }
+    </>
   );
 }
 
