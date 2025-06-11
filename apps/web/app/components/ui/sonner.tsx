a9def05a1b13dbbccdb4680771beece51cd72a3b
RiskyMH <git@riskymh.dev>
2025-06-11 21:35:19 +1000

    ooops

--- apps/web/app/components/ui/sonner.tsx
+++ apps/web/app/components/ui/sonner.tsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             className="toaster group"
             toastOptions={{
                 classNames: {
-                    toast: "group toast bg-background group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border! group-[.toaster]:shadow-lg!",
+                    toast: "group toast bg-tertiary group-[.toaster]:bg-tertiary group-[.toaster]:text-foreground group-[.toaster]:border-border! group-[.toaster]:shadow-lg!",
                     description: "group-[.toast]:text-muted-foreground!",
                     actionButton: "group-[.toast]:bg-primary! group-[.toast]:text-primary-foreground!",
                     cancelButton: "group-[.toast]:bg-muted! group-[.toast]:text-muted-foreground!",
