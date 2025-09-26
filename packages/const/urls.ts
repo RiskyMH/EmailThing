8bdc1b2c61de7bb49443f1f215588e273a28be7e
RiskyMH <git@riskymh.dev>
2025-09-27 06:48:33 +1000

    https://api.emailthing.app

--- packages/const/urls.ts
+++ packages/const/urls.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3002";
+export const API_URL = "https://api.emailthing.app";
 export const WEB_URL = "https://emailthing.app";
 
 export const DISCORD_URL = "https://discord.gg/GT9Q2Yz4VS";
