eb97c10aca6869e1bbfdcd72a3a2aadc4fe269d5
RiskyMH <git@riskymh.dev>
2025-09-30 00:06:22 +1000

    wait nvm that's not the issue

--- apps/api/src/routes/cron/clean-mail/route.ts
+++ apps/api/src/routes/cron/clean-mail/route.ts
@@ -9,7 +9,8 @@ import {
   db,
 } from "@/db";
 import { deleteFile } from "@/utils/s3";
-import { eq, inArray, lt, or, sql } from "drizzle-orm";
+import { eq, inArray, lt, not, or, sql } from "drizzle-orm";
+import { and } from "drizzle-orm";
 
 // TODO: do this "cron" better instead of still having the old vercel remains
 const every24hr = 1000 * 60 * 60 * 24;
@@ -44,16 +45,19 @@ export async function GET(request: Request) {
   });
 
   const emails = await db.query.Email.findMany({
-    where: or(
-      // get emails deleted more than 30 days ago
-      lt(Email.binnedAt, new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)),
-      // or if its part of a expired temp alias
-      tempAliases.length
-        ? inArray(
-            Email.tempId,
-            tempAliases.map((temp) => temp.id)
-          )
-        : undefined
+    where: and(
+      or(
+        // get emails deleted more than 30 days ago
+        lt(Email.binnedAt, new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)),
+        // or if its part of a expired temp alias
+        tempAliases.length
+          ? inArray(
+              Email.tempId,
+              tempAliases.map((temp) => temp.id)
+            )
+          : undefined
+      ),
+      not(eq(Email.isDeleted, true))
     ),
     columns: {
       id: true,
@@ -70,110 +74,112 @@ export async function GET(request: Request) {
     },
   });
 
-  console.log(`would delete ${emails.length} emails and ${tempAliases.length} temp aliases`);
+  console.log(
+    `would delete ${emails.length} emails and ${tempAliases.length} temp aliases`
+  );
 
-  // await Promise.all(
-  //   emails.map(async (email) => {
-  //     await deleteFile(`${email.mailboxId}/${email.id}`);
-  //     await deleteFile(`${email.mailboxId}/${email.id}/email.eml`);
-  //     await Promise.all(
-  //       email.attachments.map(async (attachment) => {
-  //         await deleteFile(
-  //           `${email.mailboxId}/${email.id}/${attachment.id}/${attachment.filename}`
-  //         );
-  //       })
-  //     );
-  //   })
-  // );
+  await Promise.all(
+    emails.map(async (email) => {
+      await deleteFile(`${email.mailboxId}/${email.id}`);
+      await deleteFile(`${email.mailboxId}/${email.id}/email.eml`);
+      await Promise.all(
+        email.attachments.map(async (attachment) => {
+          await deleteFile(
+            `${email.mailboxId}/${email.id}/${attachment.id}/${attachment.filename}`
+          );
+        })
+      );
+    })
+  );
 
-  // await db.batchUpdate([
-  //   // delete from db
-  //   db
-  //     .update(Email)
-  //     .set({
-  //       isDeleted: true,
-  //       updatedAt: new Date(),
-  //       body: "<deleted>",
-  //       subject: "<deleted>",
-  //       binnedAt: null,
-  //       categoryId: null,
-  //       givenId: null,
-  //       givenReferences: null,
-  //       html: null,
-  //       isRead: true,
-  //       isSender: false,
-  //       replyTo: null,
-  //       snippet: null,
-  //       size: 0,
-  //       isStarred: false,
-  //       // tempId: null,
-  //       createdAt: new Date(),
-  //     })
-  //     .where(
-  //       emails.length
-  //         ? inArray(
-  //             Email.id,
-  //             emails.map((email) => email.id)
-  //           )
-  //         : sql`1 = 0`
-  //     ),
+  await db.batchUpdate([
+    // delete from db
+    db
+      .update(Email)
+      .set({
+        isDeleted: true,
+        updatedAt: new Date(),
+        body: "<deleted>",
+        subject: "<deleted>",
+        binnedAt: null,
+        categoryId: null,
+        givenId: null,
+        givenReferences: null,
+        html: null,
+        isRead: true,
+        isSender: false,
+        replyTo: null,
+        snippet: null,
+        size: 0,
+        isStarred: false,
+        // tempId: null,
+        createdAt: new Date(),
+      })
+      .where(
+        emails.length
+          ? inArray(
+              Email.id,
+              emails.map((email) => email.id)
+            )
+          : sql`1 = 0`
+      ),
 
-  //   db.delete(EmailSender).where(
-  //     emails.length
-  //       ? inArray(
-  //           EmailSender.emailId,
-  //           emails.map((email) => email.id)
-  //         )
-  //       : sql`1 = 0`
-  //   ),
-  //   db.delete(EmailRecipient).where(
-  //     emails.length
-  //       ? inArray(
-  //           EmailRecipient.emailId,
-  //           emails.map((email) => email.id)
-  //         )
-  //       : sql`1 = 0`
-  //   ),
-  //   db.delete(EmailAttachments).where(
-  //     emails.length
-  //       ? inArray(
-  //           EmailAttachments.emailId,
-  //           emails.map((email) => email.id)
-  //         )
-  //       : sql`1 = 0`
-  //   ),
+    db.delete(EmailSender).where(
+      emails.length
+        ? inArray(
+            EmailSender.emailId,
+            emails.map((email) => email.id)
+          )
+        : sql`1 = 0`
+    ),
+    db.delete(EmailRecipient).where(
+      emails.length
+        ? inArray(
+            EmailRecipient.emailId,
+            emails.map((email) => email.id)
+          )
+        : sql`1 = 0`
+    ),
+    db.delete(EmailAttachments).where(
+      emails.length
+        ? inArray(
+            EmailAttachments.emailId,
+            emails.map((email) => email.id)
+          )
+        : sql`1 = 0`
+    ),
 
-  //   db
-  //     .update(TempAlias)
-  //     .set({
-  //       isDeleted: true,
-  //       createdAt: new Date(),
-  //       name: "<deleted>",
-  //       updatedAt: new Date(),
-  //       // alias: "<deleted>",
-  //     })
-  //     .where(
-  //       tempAliases.length
-  //         ? inArray(
-  //             TempAlias.id,
-  //             tempAliases.map((temp) => temp.id)
-  //           )
-  //         : sql`1 = 0`
-  //     ),
+    db
+      .update(TempAlias)
+      .set({
+        isDeleted: true,
+        createdAt: new Date(),
+        name: "<deleted>",
+        updatedAt: new Date(),
+        // alias: "<deleted>",
+      })
+      .where(
+        tempAliases.length
+          ? inArray(
+              TempAlias.id,
+              tempAliases.map((temp) => temp.id)
+            )
+          : sql`1 = 0`
+      ),
 
-  //   ...emails.map((email) =>
-  //     db
-  //       .update(Mailbox)
-  //       .set({
-  //         storageUsed: sql`${Mailbox.storageUsed} - ${email.size}`,
-  //       })
-  //       .where(eq(Mailbox.id, email.mailboxId))
-  //   ),
+    ...emails.map((email) =>
+      db
+        .update(Mailbox)
+        .set({
+          storageUsed: sql`${Mailbox.storageUsed} - ${email.size}`,
+        })
+        .where(eq(Mailbox.id, email.mailboxId))
+    ),
 
-  //   db
-  //     .delete(UserSession)
-  //     .where(lt(UserSession.refreshTokenExpiresAt, new Date())),
-  // ]);
+    db
+      .delete(UserSession)
+      .where(lt(UserSession.refreshTokenExpiresAt, new Date())),
+  ]);
 
   return Response.json({
     success: true,
