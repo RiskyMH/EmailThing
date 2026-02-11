8ca4ff5761eb1afbcaae7bb7a603d32092abdae8
RiskyMH <git@riskymh.dev>
2026-02-12 00:18:04 +1100

    cli: check bun version to avoid errors like `Bun.markdown` is undefined

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,14 +1,26 @@
 #!/usr/bin/env bun
 
+// throw nice warning if they are using an old version of Bun that doesn't support some features we use
+if (!Bun.semver.satisfies(Bun.version, ">=1.3.9")) {
+  console.error(`EmailThing CLI requires Bun v1.3.9 or higher. You are running ${Bun.version}. Please update Bun to continue.`);
+  if (process.stdin.isTTY && confirm("Upgrade bun?")) {
+    Bun.spawnSync(
+      [process.execPath, "upgrade"],
+      { stdout: "inherit", stderr: "inherit", stdin: "inherit" }
+    );
+  }
+  process.exit(1);
+}
+
 // check for logout option
-if (process.argv.includes("logout")) {
+else if (process.argv.includes("logout")) {
   const { getDB, clearAuth, resetDB } = await import("@/utils/config");
   const db = getDB();
   clearAuth(db);
   resetDB(db);
   console.log("Logged out successfully. Run again to login.");
   db.close();
-} 
+}
 
 // check for agent mode
 else if (
@@ -16,14 +28,14 @@ else if (
   (process.env.CLAUDECODE || process.env.OPENCODE || process.env.AGENT) === "1"
 ) {
   await import("./agent");
-} 
+}
 
 // warn if not running in a TTY and not in agent mode, since the CLI won't work properly
 else if (!process.stdin.isTTY || !process.stdout.isTTY) {
   console.error("This application is meant to be run in an interactive terminal.");
   console.log("Use `bunx @emailthing/cli agent` flag to run in agent mode instead.");
   process.exit(1);
-} 
+}
 
 // otherwise run the normal (interactive) CLI
 else {
