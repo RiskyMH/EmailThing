5c143b915ba118b24d67d6a74f264219d71843cd
RiskyMH <git@riskymh.dev>
2026-02-20 16:46:37 +1100

    pwa:some wird fix by undoing something that i thought fixed something else

--- apps/pwa/src/components/ui/input.tsx
+++ apps/pwa/src/components/ui/input.tsx
@@ -9,9 +9,9 @@ function Input({ className, type, asChild, ...props }: React.ComponentProps<"inp
       <Slot
         data-slot="input"
         className={cn(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground //dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 md:text-sm",
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground //dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]",
-          "has:aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 has:aria-invalid:border-destructive",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           className
         )}
         {...props}
