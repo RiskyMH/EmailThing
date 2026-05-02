79bd9dab471dfb614b6fe71e5ff475b17fe04cc2
RiskyMH <git@riskymh.dev>
2026-05-03 01:44:15 +1000

    pwa: ensure shiki doesn't get bundled through the macro Not sure what static analysis changed, but its annoying.. it really shouldn't be caring about imports from macros

--- apps/pwa/src/(app)/mailbox-config/macro.tsx
+++ apps/pwa/src/(app)/mailbox-config/macro.tsx
@@ -1,10 +1,12 @@
 import cfWorkerCodeText from "@/../public/cloudflare-worker.js" with { type: "text" };
-import { codeToHtml } from "shiki";
+// import { codeToHtml } from "shiki";
 
 export async function cloudflareWorkerCode() {
   if (typeof window === "undefined" && process?.platform === "win32")
     return cfWorkerCodeText as any as string;
 
+  // sus things to ensure bun doesn't include it in module graph
+  const { codeToHtml } = await import("shiki".slice()) as typeof import("shiki");
   return codeToHtml(cfWorkerCodeText as any as string, {
     lang: "javascript",
     theme: "github-dark",
